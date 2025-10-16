efd75c06c2860ac80a4c92898d79caf833d41fae
philipnickel <tipkik@hotmail.com>
2025-10-16 21:16:57 +0200

    larger images

--- env_hpc.sh
+++
@@ -1,9 +0,0 @@
-# Source this file on HPC: source env_hpc.sh
-# Loads required modules for development/testing on HPC
-
-module load python3/3.11.1
-module load mpi/5.0.8-gcc-13.4.0-binutils-2.44
-
-echo "✓ HPC modules loaded"
-echo "  - Python 3.11.1"
-echo "  - MPI 5.0.8"
