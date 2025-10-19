8f16ca0d7ee37b8535a501bbbb91511aab55fdb1
Philip Korsager Nickel <philipnickel@Mac.utopia.net>
2025-10-19 19:42:46 +0200

    more

--- job_scripts/generated/scaling_mult_host.sh
+++ job_scripts/generated/scaling_mult_host.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#BSUB -J scaling_mult_host[1-8]
+#BSUB -J scaling_mult_host[1-20]
 #BSUB -q hpcintro
-#BSUB -n 70
+#BSUB -n 50
 #BSUB -W 00:10
 #BSUB -R "span[ptile=10]"
 #BSUB -R "rusage[mem=5GB]"
